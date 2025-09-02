b25af102fd220309c783cf79de1b0c64ef12fd5a
Samuel Ribas <samuelfribas@gmail.com>
2025-09-02 20:42:10 -0300

    Update Travessia-crossing.c

--- BST Classes/Travessia-crossing.c
+++ BST Classes/Travessia-crossing.c
@@ -1,30 +1,37 @@
 #include<stdio.h>
 
-typedef struct noABP
-{
-    int data;
-    struct noABP *left, *right;
-}TnoABP;
+typedef struct TreeNode {
+    int key;
+    struct TreeNode *left, *right;
+} TreeNode;
+
 
 /*
     In portuguese: Travessia->> Imprime todos os valores chave de uma árvore binária dada
     In english: crossing->> Print all key values ​​of a given binary tree 
 */
 
-void preorder(TnoABP *root)
-{
-    if(root){
-        printf("%d\n", root->data); // visitação / visitation
-        preorder(root->left);
-        preorder(root->right); 
-    }
+
+void preorder(TreeNode *r) {      // Root, Left, Right
+    if (!r) return;
+
+    printf("%d ", r->key);      // visitação / visitation
+    preorder(r->left);
+    preorder(r->right);
 }
 // print in order
-void inorder(TnoABP *root)
-{
-    if(root){
-        preorder(root->left);
-        printf("%d\n", root->data);
-        preorder(root->right); 
-    }
+void inorder(TreeNode *r) {       // Left, Root, Right
+    if (!r) return;
+
+    inorder(r->left);
+    printf("%d ", r->key);        // visitação / visitation
+    inorder(r->right);
+}
+
+void postorder(TreeNode *r) {     // Left, Right, Root
+    if (!r) return;
+
+    postorder(r->left);
+    postorder(r->right);
+    printf("%d ", r->key);         // visitação / visitation
 }
