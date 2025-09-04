0fa82e6ccf457dec4eca71d504cf7777fc4dc066
Samufasaa <samuelfribas@gmail.com>
2025-09-04 13:52:17 -0300

    function swap node

---
+++ BST Classes/function_swap_node.c
@@ -0,0 +1,16 @@
+// função substitui dados para remoção
+// function swaping node for removal
+/*
+void substituiDados(Node **PP, TNoABP **pps) 
+{
+    TNoABP *esq = (*pp)->esq;
+    TNoABP *dir = (*pp)->dir;
+    memcpy(*pp, *pps , sizeof(TnoABP));
+    (*pp)->esq = esq;
+    (*pp)->dir = dir;
+
+    //dessa forma pode-se mexer a vontade na struct e mantem o foco em remover o nó folha
+    // fazer remova todos os nó descendentes de menor valor chave de uma ab 
+    
+}
+*/
